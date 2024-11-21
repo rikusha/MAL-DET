b13d5a807ac07154bcfd5b66d50218d85ea4ddf2
rikusha <ritishasndt@gmail.com>
2024-11-21 21:43:31 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import joblib
+import sklearn
 
 with open('malware_classifier.pkl', 'rb') as f:
     model = joblib.load(f)
