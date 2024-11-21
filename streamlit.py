537da23eea99a85439d48db972db8474a04a06f0
rikusha <ritishasndt@gmail.com>
2024-11-21 09:20:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,14 +38,20 @@ with col2:
     st.header("Non-Infected Files")
     st.dataframe(df[df['legitimate'] == 0])
 
-plt.figure(figsize=(2,2), dpi=300)  #figure size and dpi
-fig, ax = plt.subplots()
+
+fig, ax = plt.subplots(figsize=(5, 3))  # Adjust figure size
+labels = ['Non-Infected', 'Infected']
 fig.patch.set_facecolor('black')
 labels = ['Non-Infected', 'Infected']
 colors = ['#85586F', '#B7D3DF']
 font = {'family': 'serif',
             'color':  'white',
              'size': 5}
+
+
+sizes = [malData['label'].value_counts()[0], malData['label'].value_counts()[1]]
+ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+ax.axis('equal')
     
 sizes = [df['legitimate'].value_counts()[0], df['legitimate'].value_counts()[1]]
 ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, textprops=font)
