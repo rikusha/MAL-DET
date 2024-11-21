65d55e611705f49cbb1fb3d3db00f3bcc8d76ae7
rikusha <ritishasndt@gmail.com>
2024-11-21 09:15:21 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,24 +38,19 @@ with col2:
     st.header("Non-Infected Files")
     st.dataframe(df[df['legitimate'] == 0])
 
-col1, col2 = st.columns([2,1])
-
-with col1:
-    plt.figure(figsize=(0.75, 0.75), dpi=300)  #figure size and dpi
-    fig, ax = plt.subplots()
-    fig.patch.set_facecolor('black')
-    labels = ['Non-Infected', 'Infected']
-    colors = ['#85586F', '#B7D3DF']
-    font = {'family': 'serif',
+plt.figure(figsize=(0.75, 0.75), dpi=300)  #figure size and dpi
+fig, ax = plt.subplots()
+fig.patch.set_facecolor('black')
+labels = ['Non-Infected', 'Infected']
+colors = ['#85586F', '#B7D3DF']
+font = {'family': 'serif',
             'color':  'white',
              'size': 3}
     
-    sizes = [df['legitimate'].value_counts()[0], df['legitimate'].value_counts()[1]]
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, textprops=font)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.markdown("<div style='text-align:center'>", unsafe_allow_html=True)
-    st.pyplot(fig)
-    st.markdown("</div>", unsafe_allow_html=True)
+sizes = [df['legitimate'].value_counts()[0], df['legitimate'].value_counts()[1]]
+ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, textprops=font)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.markdown("<div style='text-align:center'>", unsafe_allow_html=True)
+st.pyplot(fig)
+st.markdown("</div>", unsafe_allow_html=True)
     
-with col2: 
-    print("    ")
